7f0c400aae6d64d16a05ec96243e9d8c83fadf74
Andrey Yamanov <tenphi@gmail.com>
2026-04-16 10:56:35 +0200

    fix: search box position

--- app/docs/components/SearchDialog.tsx
+++ app/docs/components/SearchDialog.tsx
@@ -110,7 +110,7 @@ const Backdrop = tasty({
     zIndex: 1000,
     fill: '#black.50',
     display: 'flex',
-    placeContent: 'start center',
+    placeItems: 'center',
     padding: '10vh 2x 2x',
   },
 });
